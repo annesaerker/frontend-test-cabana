068dc69ecdc25559150273fdd6b342f4eb21520a
Anne Saerker <annemai11@hotmail.com>
2019-02-17 12:03:59 +0100

    pop-up modal implemented

--- src/js/script.js
+++ src/js/script.js
@@ -10,3 +10,44 @@ $(document).ready(function() {
     stopOnHover: true
   });
 });
+
+
+(function($) {
+    
+  $.fn.bmdIframe = function( options ) {
+      var self = this;
+      var settings = $.extend({
+          classBtn: '.bmd-modalButton',
+          defaultW: 640,
+          defaultH: 360
+      }, options );
+    
+      $(settings.classBtn).on('click', function(e) {
+        var allowFullscreen = $(this).attr('data-bmdVideoFullscreen') || false;
+        
+        var dataVideo = {
+          'src': $(this).attr('data-bmdSrc'),
+          'height': $(this).attr('data-bmdHeight') || settings.defaultH,
+          'width': $(this).attr('data-bmdWidth') || settings.defaultW
+        };
+        
+        if ( allowFullscreen ) dataVideo.allowfullscreen = "";
+        
+        $(self).find("iframe").attr(dataVideo);
+      });
+    
+      this.on('hidden.bs.modal', function(){
+        $(this).find('iframe').html("").attr("src", "");
+      });
+    
+      return this;
+  };
+
+})(jQuery);
+
+
+
+
+jQuery(document).ready(function(){
+jQuery("#myModal").bmdIframe();
+});
