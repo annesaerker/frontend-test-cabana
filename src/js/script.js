864ad935def4027feda298a8eafc10e059062e2d
Anne Saerker <annemai11@hotmail.com>
2019-02-19 04:29:42 +0100

    overall adjustments

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,3 @@
-console.log("Owl caro");
-
 $(document).ready(function() {
   $(".owl-carousel").owlCarousel({
     items: 1,
@@ -21,42 +19,36 @@ $(document).ready(function() {
   
     e.preventDefault();
   });
-});
 
-
-(function($) {
-    
   $.fn.bmdIframe = function( options ) {
-      var self = this;
-      var settings = $.extend({
-          classBtn: '.bmd-modalButton',
-          defaultW: 640,
-          defaultH: 360
-      }, options );
-    
-      $(settings.classBtn).on('click', function(e) {
-        var allowFullscreen = $(this).attr('data-bmdVideoFullscreen') || false;
-        
-        var dataVideo = {
-          'src': $(this).attr('data-bmdSrc'),
-          'height': $(this).attr('data-bmdHeight') || settings.defaultH,
-          'width': $(this).attr('data-bmdWidth') || settings.defaultW
-        };
-        
-        if ( allowFullscreen ) dataVideo.allowfullscreen = "";
-        
-        $(self).find("iframe").attr(dataVideo);
-      });
-    
-      this.on('hidden.bs.modal', function(){
-        $(this).find('iframe').html("").attr("src", "");
-      });
-    
-      return this;
+    var self = this;
+    var settings = $.extend({
+        classBtn: '.bmd-modalButton',
+        defaultW: 640,
+        defaultH: 360
+    }, options );
+  
+    $(settings.classBtn).on('click', function(e) {
+      var allowFullscreen = $(this).attr('data-bmdVideoFullscreen') || false;
+      
+      var dataVideo = {
+        'src': $(this).attr('data-bmdSrc'),
+        'height': $(this).attr('data-bmdHeight') || settings.defaultH,
+        'width': $(this).attr('data-bmdWidth') || settings.defaultW
+      };
+      
+      if ( allowFullscreen ) dataVideo.allowfullscreen = "";
+      
+      $(self).find("iframe").attr(dataVideo);
+    });
+  
+    this.on('hidden.bs.modal', function(){
+      $(this).find('iframe').html("").attr("src", "");
+    });
+  
+    return this;
   };
-
-})(jQuery);
-
+});
 
 jQuery(document).ready(function(){
   jQuery("#myModal").bmdIframe();
